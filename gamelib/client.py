c58a233b48712d69f976e6669094d39f34f76088
Patrick Devine <pdevine@sonic.net>
2009-08-25 10:02:32 -0700

    animate tiles

--- gamelib/client.py
+++ gamelib/client.py
@@ -58,32 +58,63 @@ class Tile(pyglet.sprite.Sprite):
         self.x = x
         self.y = y
 
-        #self.rotation = 0
+        self.moving = 0
+
         self.rotation = tileRotation * 90
 
 class Board(object):
     def __init__(self):
-        self.tiles = pyglet.graphics.Batch()
+        self.tileBatch = pyglet.graphics.Batch()
         self.sprites = []
 
-        for x in range(0, 12):
-            for y in range(0, 9):
-                self.sprites.append(Tile(x * 81 + OFFSET_X,
-                                         y * 81 + OFFSET_Y,
-                                         tileType=random.randint(1, 3),
-                                         tileRotation=random.randint(0, 3),
-                                         batch=self.tiles))
+        for y in range(0, 9):
+            tempRow = []
+            for x in range(0, 12):
+                tempRow.append(Tile(x * 81 + OFFSET_X,
+                                    y * 81 + OFFSET_Y,
+                                    tileType=random.randint(1, 3),
+                                    tileRotation=random.randint(0, 3),
+                                    batch=self.tileBatch))
+            self.sprites.append(tempRow)
+
+        pyglet.clock.schedule(self.update)
+
+    def moveRow(self, row, direction):
+        assert direction in [board.EAST, board.WEST]
+
+        for tile in self.sprites[row]:
+            tile.moving = direction
+
+    def moveColumn(self, column, direction):
+        assert direction in [board.NORTH, board.SOUTH]
+
+        for row in range(9):
+            self.sprites[row][column].moving = direction
+
+    def update(self, dt):
+        for row in self.sprites:
+            for tile in row:
+                if tile.moving == board.EAST:
+                    tile.x -= dt * 40
+                elif tile.moving == board.WEST:
+                    tile.x += dt * 40
+                elif tile.moving == board.NORTH:
+                    tile.y += dt * 40
+                elif tile.moving == board.SOUTH:
+                    tile.y -= dt * 40
 
     def draw(self):
-        self.tiles.draw()
+        self.tileBatch.draw()
 
 title = Title()
-board = Board()
+gameBoard = Board()
+#gameBoard.moveRow(2, board.WEST)
+gameBoard.moveColumn(2, board.SOUTH)
 
 @window.event
 def on_draw():
     window.clear()
-    board.draw()
+    gameBoard.draw()
     title.draw()
 
 pyglet.app.run()
